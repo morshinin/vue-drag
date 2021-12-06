d386dd5fcc8b1700265b066a3a13d95d40450bab
morshinin <314159265absurdus@gmail.com>
2021-12-06 03:15:50 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="min-h-screen w-screen
+  bg-gray-200 flex items-center justify-center">
+    <Draggable tag="ul" ghost-class="moving-card" filter=".action-button" class="w-full max-w-md" :list="users" :animation="200">
+      <li 
+        v-for="user in users" 
+        :key="user.id"
+        class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move" 
+      >
+      <div class="flex items-center w-full">
+        <img :src="user.avatar" :alt="user.name" class="w-10 h-10 rounded-full">
+        <p class="ml-2 text-gray-700 font-semibold tracking-wide">
+          {{ user.name }}
+        </p>
+        <div class="flex ml-auto">
+          <button
+            aria-label="Edit user"
+            class="p-1 focus:outline-none focus:shadow-outline text-green-500 hover:text-green-600"
+            @click="onEdit(user)"
+          >
+            <EditIcon />
+          </button>
+          <button
+            aria-label="Delete user"
+            class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600"
+            @click="onDelete(user)"
+          >
+            <Trash2Icon />
+          </button>
+        </div>
+      </div>
+      </li>  
+    </Draggable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { EditIcon, Trash2Icon } from "vue-feather-icons";
+import Draggable from 'vuedraggable';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Adrian Schubert",
+          avatar: "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
+        },
+        {
+          id: 2,
+          name: "Violet Gates",
+          avatar: "https://pickaface.net/gallery/avatar/freud51c8b3f65e7dc.png"
+        },
+        {
+          id: 3,
+          name: "Steve Jobs",
+          avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
+        },
+        {
+          id: 4,
+          name: "Yassine Smith",
+          avatar: "https://pickaface.net/gallery/avatar/unr_yassine_191124_2012_3gngr.png"
+        },
+        {
+          id: 5,
+          name: "Senior Saez",
+          avatar: "https://pickaface.net/gallery/avatar/elmedinilla541c03412955c.png"
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    EditIcon,
+    Trash2Icon,
+    Draggable,
+  },
+
+  methods: {
+    onEdit(user) {
+      console.log(`Editing ${user.name}`);
+    },
+    onDelete(user) {
+      console.log(`Deleting ${user.name}`);
+    }
   }
 }
 </script>
@@ -23,6 +95,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.moving-card {
+  @apply opacity-50 bg-gray-100 border border-blue-500;
 }
 </style>
